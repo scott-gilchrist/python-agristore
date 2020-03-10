468faf43aa38e285eddb6af1d1030a0e2585f103
Scott Gilchrist <sgilchrist271@gmail.com>
2020-03-10 16:18:45 +0000

    updating to add a registration form

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+# Create app objects
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Create the login manager
+login = LoginManager(app)
+login.login_view = 'login'
+
+# Create database objects
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
